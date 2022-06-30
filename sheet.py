61393c6ebc76cd77ba13d4f5011243431bf4d504
tanuj1221 <tcchavan999@gmail.com>
2022-06-30 14:04:03 +0530

    make

--- sheet.py
+++ sheet.py
@@ -6,7 +6,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+
 from google.oauth2 import service_account
 import numpy as np
 import pandas as pd
