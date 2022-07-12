a42065c1c23ca2595b284a87f4e3258f5b4f436a
tanuj1221 <tcchavan999@gmail.com>
2022-07-12 17:35:09 +0530

    'f'

--- app.py
+++ app.py
@@ -44,38 +44,40 @@ def sem():
 
 
 
+    try:
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        SERVICE_ACCOUNT_FILE = 'key.json'
 
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    SERVICE_ACCOUNT_FILE = 'key.json'
+        credentials=None
+        credentials = service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-    credentials=None
-    credentials = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-    # If modifying these scopes, delete the file token.json.
-
-
-    # The ID and range of a sample spreadsheet.
-    SAMPLE_SPREADSHEET_ID = '1X8lKo1meN11XHHMDVECpw-Ek-nKhhDg4U7iGU1VwmCs'
-    service = build('sheets', 'v4', credentials=credentials)
+        # If modifying these scopes, delete the file token.json.
 
 
-    df_list = pd.read_html(r) # this parses all the tables in webpages to a list
-    df = df_list[0]
-   
-    df.replace(np.nan, '', inplace=True)
-    rangeAll = '{0}!A1:Z'.format('DF')
-    body = {}
-    resultClear = service.spreadsheets( ).values( ).clear( spreadsheetId=SAMPLE_SPREADSHEET_ID, range=rangeAll,
-                                                        body=body ).execute( )
-    response_date = service.spreadsheets().values().append(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID,
-        valueInputOption='RAW',
-        range='df!A1',
-        body=dict(
-            majorDimension='ROWS',
-            values=df.T.reset_index().T.values.tolist())
-    ).execute()
+        # The ID and range of a sample spreadsheet.
+        SAMPLE_SPREADSHEET_ID = '1X8lKo1meN11XHHMDVECpw-Ek-nKhhDg4U7iGU1VwmCs'
+        service = build('sheets', 'v4', credentials=credentials)
+
+
+        df_list = pd.read_html(r) # this parses all the tables in webpages to a list
+        df = df_list[0]
+    
+        df.replace(np.nan, '', inplace=True)
+        rangeAll = '{0}!A1:Z'.format('DF')
+        body = {}
+        resultClear = service.spreadsheets( ).values( ).clear( spreadsheetId=SAMPLE_SPREADSHEET_ID, range=rangeAll,
+                                                            body=body ).execute( )
+        response_date = service.spreadsheets().values().append(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID,
+            valueInputOption='RAW',
+            range='df!A1',
+            body=dict(
+                majorDimension='ROWS',
+                values=df.T.reset_index().T.values.tolist())
+        ).execute()
+    except:
+        print(r)
     driver.close()
 
 def asx():
