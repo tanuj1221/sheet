a02655f6f1afdad385918906a81cbe8ea98a2143
tanuj1221 <tcchavan999@gmail.com>
2022-07-12 17:13:33 +0530

    'f'

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def sem():
     import os
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
